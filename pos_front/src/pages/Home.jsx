f62c4aa4fdc4449cb098188c07c65ebb5105d6e3
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-11-18 17:43:01 +0100

    Home modified

--- pos_front/src/pages/Home.jsx
+++ pos_front/src/pages/Home.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <section className="bg-[#1f1f1f]  h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* Left */}
+      <div className="flex-3">
+        <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+          <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6} />
+        </div>
+        <RecentOrders />
+      </div>
+      {/* Right */}
+      <div className="flex-2">
+        <PopularDishes />
+      </div>
+      <BottomNav />
+    </section>
+  );
 }
 
 export default Home
